6b5a9762e3788b9e857079beabe576d52bfd1959
Guilherme Solon de Menezes <guisolon.tec@gmail.com>
2025-05-12 17:00:37 -0300

    Update POO.java

--- oi.java
+++ oi.java
@@ -1,7 +1,47 @@
-public class oi{
-    public static void main(String[] args) {
-        
-        System.out.println("Olá crey crey");
+package com.projeto;
 
-    }
+import java.util.Scanner;
+
+
+
+public class ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApplication.class, args);
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter number");
+		
+		int a = scanner.nextInt();
+		int b= scanner.nextInt();
+		int c= scanner.nextInt();
+		
+		int maior = max(a,b,c);
+		
+		showResult(maior);
+		
+		scanner.close();
+	}
+	
+public static int max(int x, int y, int z) {
+	
+	int aux; 
+	
+	if(x > y && x > z) {
+		aux = x;
+	} else if(y > z) {
+		aux = y;
+	} else {
+		aux = z;
+	}
+	
+	return aux;
+	
+}
+
+public static void showResult(int value) {
+  System.out.println(value);
+}
+
 }
